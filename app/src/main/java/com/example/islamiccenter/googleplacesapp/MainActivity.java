8a42a3dbb7bff6cfa13375381f5f5756e0f24929
hesham <heshamhamdi77889@gmail.com>
2017-10-11 15:32:37 +0200

    intialz

--- app/src/main/java/com/example/islamiccenter/googleplacesapp/MainActivity.java
+++ app/src/main/java/com/example/islamiccenter/googleplacesapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.islamiccenter.googleplacesapp;
 
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -14,6 +16,8 @@ import com.example.islamiccenter.googleplacesapp.PlacesModel.PlacesModel;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+
+import org.json.JSONException;
 
 import java.io.IOException;
 
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     PlacesModel [] placemodels;
     Gson gson = new Gson();
     PlacesAdapter placesadapter;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,36 +44,47 @@ public class MainActivity extends AppCompatActivity {
         name = (EditText)findViewById(R.id.editText_name);
         search= (Button)findViewById(R.id.button_search);
         listView=(ListView)findViewById(R.id.ListView_places);
-    }
-    OkHttpClient client = new OkHttpClient();
+        progressDialog=new ProgressDialog(MainActivity.this);
 
-    String run(String url) throws IOException {
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
+        MyAsyncTask myAsyncTask = new MyAsyncTask();
+        String url ="http://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyBp4c8yTUU9vhYmU73ZMHBOWHIinW5q0b8"
 
-        Response response = client.newCall(request).execute();
-        return response.body().string();
+        myAsyncTask.execute(url);
     }
 
-    private class MyAsyncTask extends AsyncTask<String , Void , Bitmap> {
-        protected void onPreExecute(String response) {
 
-            JsonObject jsonObject=new JsonObject(response);
-            JsonArray jsonArray=jsonObject.getAsJsonArray("results");
-            placemodels = gson.fromJson(jsonArray.toString(), PlacesModel[].class);
+    private class MyAsyncTask extends AsyncTask<String , Void ,PlacesModel[] > {
+        protected void onPreExecute() {
 
-            // Runs on the UI thread before doInBackground
-            // Good for toggling visibility of a progress indicator
-            progressBar.setVisibility(ProgressBar.VISIBLE);
+        }
+        OkHttpClient client = new OkHttpClient();
+
+        String run(String url) throws IOException {
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            Response response = client.newCall(request).execute();
+            return response.body().string();
         }
 
-        protected Bitmap doInBackground(String... strings) {
-            placesadapter=new PlacesAdapter(MainActivity.this ,placemodels);
-            listView.setAdapter(placesadapter);
-            // Some long-running task like downloading an image.
-            Bitmap = downloadImageFromUrl(strings[0]);
-            return someBitmap;
+        protected PlacesModel[] doInBackground(String... url) {
+            try {
+                String s = run(url[0]);
+                Log.d("yarb tfla7", s);
+                JsonObject jsonObject = new JsonObject(s);
+                JsonArray jsonArray = jsonObject.getAsJsonArray("results");
+                placemodels = gson.fromJson(jsonArray.toString(), PlacesModel[].class);
+                return placemodels;
+
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
         }
 
 //        protected void onProgressUpdate(Progress... values) {
@@ -77,12 +93,13 @@ public class MainActivity extends AppCompatActivity {
 //            progressBar.setProgress(values[0]);
 //        }
 
-        protected void onPostExecute(Bitmap result) {
-            // This method is executed in the UIThread
-            // with access to the result of the long running task
-            imageView.setImageBitmap(result);
-            // Hide the progress bar
-            progressBar.setVisibility(ProgressBar.INVISIBLE);
+        protected void onPostExecute(PlacesModel[]placesModels) {
+            progressDialog.dismiss();
+            if(placesModels!=null) {
+                placesadapter = new PlacesAdapter(MainActivity.this, placemodels);
+                listView.setAdapter(placesadapter);
+
+            }
         }
     }
 }
